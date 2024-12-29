d7193b9ed382e8ed4804142c67a4cada6ee612ce
Asif Javed <asifgulu88@gmail.com>
2024-12-29 21:38:28 +0500

    Logo

--- app/ui/next-logo.tsx
+++ app/ui/next-logo.tsx
@@ -6,7 +6,7 @@ export default function NextLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <p className="text-[40px]">MyNext</p>
+      <p className="text-[40px]">Code</p>
       <LightBulbIcon className="h-12 w-12 rotate-[15deg]" />
     </div>
   );
